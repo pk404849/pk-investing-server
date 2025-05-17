a97d6a83fc155929a2015b32c764f47e862437fe
pk404849 <pk404849@gmail.com>
2025-05-17 10:41:10 +0530

    first commit

---
+++ src/main/java/com/pk/investing/server/PkInvestingServerApplication.java
@@ -0,0 +1,14 @@
+package com.pk.investing.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkInvestingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkInvestingServerApplication.class, args);
+		System.out.println("======== main method completed successfully ==========");
+	}
+
+}
---
+++ src/main/java/com/pk/investing/server/controller/rest/kafka/KafkaController.java
@@ -0,0 +1,22 @@
+package com.pk.investing.server.controller.rest.kafka;
+
+import com.pk.investing.server.service.kafka.KafkaProducerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+
+    private final KafkaProducerService kafkaProducerService;
+
+    public KafkaController(KafkaProducerService kafkaProducerService) {
+        this.kafkaProducerService = kafkaProducerService;
+    }
+
+    @GetMapping("/send")
+    public String sendMessage(@RequestParam String message) {
+        kafkaProducerService.sendMessage(message);
+        return "Message sent successfully";
+    }
+}
