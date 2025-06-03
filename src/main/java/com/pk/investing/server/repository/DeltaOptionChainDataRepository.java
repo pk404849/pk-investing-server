a1cc17d3540a75e30bf05f55e57e7899af5a572f
Kumar <prince.n.kumar@capgemini.com>
2025-06-03 16:00:56 +0530

    commented dependncy

--- src/main/java/com/pk/investing/server/repository/DeltaOptionChainDataRepository.java
+++ src/main/java/com/pk/investing/server/repository/DeltaOptionChainDataRepository.java
@@ -3,8 +3,8 @@ package com.pk.investing.server.repository;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +13,7 @@ import com.pk.investing.server.entity.DeltaOptionChainData;
 @Repository
 public interface DeltaOptionChainDataRepository extends JpaRepository<DeltaOptionChainData, Integer> {
 
-	@Query("SELECT * FROM DeltaOptionChainData delta WHERE delta.createdTime:=localDateTime")
+	@Query("SELECT delta* FROM DeltaOptionChainData delta WHERE delta.createdTime:=localDateTime")
 	public List<DeltaOptionChainData> findByCreatedTime(@Param("localDateTime") LocalDateTime localDateTime);
 
 }
