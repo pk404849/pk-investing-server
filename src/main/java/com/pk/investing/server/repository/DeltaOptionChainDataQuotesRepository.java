f31297d2a5200f6832f873fe3c70d2e004dd90ca
Kumar <prince.n.kumar@capgemini.com>
2025-06-03 12:59:11 +0530

    add repository annotation

--- src/main/java/com/pk/investing/server/repository/DeltaOptionChainDataQuotesRepository.java
+++ src/main/java/com/pk/investing/server/repository/DeltaOptionChainDataQuotesRepository.java
@@ -1,9 +1,11 @@
 package com.pk.investing.server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.pk.investing.server.entity.DeltaOptionChainDataQuotes;
 
+@Repository
 public interface DeltaOptionChainDataQuotesRepository extends JpaRepository<DeltaOptionChainDataQuotes, Integer> {
 
 }
