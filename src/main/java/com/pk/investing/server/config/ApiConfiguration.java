861bab6512b6f3b08677308e63ed3d306df9556f
pk404849 <pk404849@gmail.com>
2025-06-07 01:18:29 +0530

    cross configuration

--- src/main/java/com/pk/investing/server/config/ApiConfiguration.java
+++ src/main/java/com/pk/investing/server/config/ApiConfiguration.java
@@ -3,6 +3,8 @@ package com.pk.investing.server.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ApiConfiguration {
@@ -11,5 +13,16 @@ public class ApiConfiguration {
 		return new RestTemplate();
 	}
 	
-	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+	    return new WebMvcConfigurer() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/**")
+	                .allowedOrigins("http://13.53.206.50:4200")
+	                .allowedMethods("GET", "POST", "PUT", "DELETE")
+	                .allowedHeaders("*");
+	        }
+	    };
+	}
 }
